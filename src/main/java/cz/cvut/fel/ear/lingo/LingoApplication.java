637f25cc1348e7485dfa590c98a5ba30f2dcd7ec
Mukan Atazhanov <atazhanovmukan@gmail.com>
2022-04-28 23:24:50 +0200

    hot fix of stupid merge request xd

--- src/main/java/cz/cvut/fel/ear/lingo/LingoApplication.java
+++ src/main/java/cz/cvut/fel/ear/lingo/LingoApplication.java
@@ -2,8 +2,10 @@ package cz.cvut.fel.ear.lingo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class LingoApplication {
     public static void main(String[] args) {
         SpringApplication.run(LingoApplication.class, args);
